1e9fd2906cc96a231fd8cf001b1482e4acd3a38d
Thiago Fagundes <thiago.fagundes@cilia.com.br>
2022-02-07 15:54:52 -0300

    add optional kind opitional and comment contact/model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,20 +1,20 @@
 class Contact < ApplicationRecord
-    belongs_to :kind, optinal: true
+    belongs_to :kind, optional: true
 
-    def author
-        "Thiago Fagundes"
-    end
+    # def author
+    #     "Thiago Fagundes"
+    # end
 
     # def kind_description
     #     self.kind.description
     # end
 
-    def as_json(options={})
-        super(
-        root: true,
-        methods: :author,
-        #methods: [:kind_description, :author],
-        include: { kind: { only: :description }}
-        )
-    end
+    # def as_json(options={})
+    #     super(
+    #     root: true,
+    #     methods: :author,
+    #     #methods: [:kind_description, :author],
+    #     include: { kind: { only: :description }}
+    #     )
+    # end
 end
