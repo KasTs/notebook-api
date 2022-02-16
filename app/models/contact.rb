ab84228bbd516b9b87704fe42990dee6f746e352
Thiago Fagundes <thiago.fagundes@cilia.com.br>
2022-02-16 16:59:30 -0300

    remove code coment

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,33 +1,11 @@
 class Contact < ApplicationRecord
     #association
-    belongs_to :kind#, optional: true
-    has_many  :phones
+    belongs_to :kind
+    has_many :phones
 
     def as_json(options={})
         h = super(options)
         h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
         h
     end
-
-
-    # def birthdate_br
-    #     I18n.l(self.birthdate) unless self.birthdate.blank?
-    # end
-
-    # def author
-    #     "Thiago Fagundes"
-    # end
-
-    # def kind_description
-    #     self.kind.description
-    # end
-
-    # def as_json(options={})
-    #     super(
-    #     root: true,
-    #     methods: :author,
-    #     #methods: [:kind_description, :author],
-    #     include: { kind: { only: :description }}
-    #     )
-    # end
 end
