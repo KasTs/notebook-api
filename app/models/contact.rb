dcb9fde37c3e4d8d68278822f884d1f362683586
Thiago Fagundes <thiago.fagundes@cilia.com.br>
2022-02-21 09:42:00 -0300

    remove def model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,10 +3,4 @@ class Contact < ApplicationRecord
     belongs_to :kind
     has_many :phones
     accepts_nested_attributes_for :phones
-
-    def as_json(options={})
-        h = super(options)
-        h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
-        h
-    end
 end
